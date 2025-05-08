4af2a01fb355d49e753d00e3d71da0c4290f4284
wagerc97 <wager.clemens@gmail.com>
2025-05-08 23:44:44 +0200

    wip

--- hysteresis/run.py
+++ hysteresis/run.py
@@ -30,30 +30,40 @@ ax.set(xlabel='Hpar', ylabel='Hperp')
 ax.set_aspect('equal')
 ax.set_xlim(-Hc, Hc)
 ax.set_ylim(-Hc, Hc)
+#> add title
+plt.title(r"Complete Hysteresis Curve for field angle $\gamma$")
 
-# TODO plot analytical Stoner Wohlfarth asteroid
-ax.plot(*Hpars, *Hperps)  #> We plot perpendicular vs parallel field
 
-### TODO comment if plot exercise is done to move on to next task
-plt.ioff()
-plt.show()
 
-print(f"plot is ready ")
-exit()
-###
+def abc():
+
+    # TODO plot analytical Stoner Wohlfarth asteroid
+    ax.plot(..., ...)  #> We plot perpendicular vs parallel field
+
+    ### TODO comment if plot exercise is done to move on to next task
+    plt.ioff()
+    plt.show()
+
+    print(f"plot is ready ")
+    #exit()
+abc()
+
 
 # add plot with empty values for Hpar and Hperp
 sw, = ax.plot(Hpars, Hperps)
+
 for alpha in angles:
     print(alpha)
-    phi = np.pi
+    phi = np.pi  #> what is this
 
     # TODO find critical field H for angle alpha
-    # Hpar  = H * cos(alpha)
-    # Hperp = H * sin(alpha)
+    Hpar  = -Hc * np.cos(alpha) ** 3
+    Hperp = Hc * np.sin(alpha) ** 3
     # Store values of critical field in variables "Hpar" and "Hperp"
     #
     # tip: use minimize(xxx, method = "L-BFGS-B")
+    mini = minimize(energy, 100, args=(phi, Hpar, Hperp))
+    print(f"mini: {mini}")
 
     # Append critical field components and update plot
     Hpars.append(Hpar)
