c273fd3d50cb1f06e3e942b3d9991170a7871f2f
wagerc97 <wager.clemens@gmail.com>
2025-05-15 16:35:10 +0200

    my try

--- implement_Hext/exercise/fd/external.py
+++ implement_Hext/exercise/fd/external.py
@@ -6,15 +6,26 @@ class ExternalField(object):
         self._mesh = mesh
         self._Ms = material["Ms"]
         self._h = np.zeros(mesh.n + (3,))
-        self._h[:,:,:,:] = np.array(h)
+        self._h[:,:,:,:] = np.array(h)      # h is a 4D tensor 
 
     def h(self, t, m):
         return self._h
 
     def E(self, t, m):
-        # TODO
-        # Implement Zeeman energy
-        # - mu_0 \int (M, H) dx
-        #raise NotImplementedError
-        
+        # TODO: Implement Zeeman energy from the field (the field is given in the class)
+        # - mu_0 \integral (M, H) dx
+        # we need the scalar product of m and H
+
+        # The first 3 index of m are the components (x,y,z) and the last index is the index of the cell
+        #all_cells = np.sum(m[i, j, k, :])
+
+        # define cell volume
+        V = self._mesh.cell_volume
+        eff_material = constants.mu_0 * self._Ms
+
+        E = 0
+        for i in m[3]:  
+            E += (eff_material * np.dot(m[:, :, :, i], self._h))
+
+        return - V * E
     
