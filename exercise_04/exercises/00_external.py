2666696708883805ca99ea99d57eb6faef0b3d01
wagerc97 <wager.clemens@gmail.com>
2025-05-22 09:52:42 +0200

    comments added

--- exercise_04/exercises/00_external.py
+++ exercise_04/exercises/00_external.py
@@ -12,11 +12,11 @@ material = {
         "Ms": 8e5,
         "A": 1.3e-11,
         "K": 1e4,
-        "K_axis": (0,0,1)
+        "K_axis": (0,0,1)  # anisotropy axis points in z-direction
         }
 
 # initialize external field pointing in y-direction
-external = ExternalField(mesh, material, (0,1,0))
+external = ExternalField(mesh, material, (0,1,0))  # apply external field in y-direction
 
 # initialize magnetization pointing in x=direction
 m0 = np.zeros(n + (3,))
@@ -31,6 +31,6 @@ write_vtr(m0, str(result_path/"m_start"), mesh)
 print("wrote initial state to filepath: ", result_path/"m_start")
 print("Minimizing energy...")
 minimizer = Minimizer([external])
-m = minimizer.minimize(m0)
+m = minimizer.minimize(m0, verbose=True)
 write_vtr(m, str(result_path/"m_relaxed"), mesh)
 print("wrote relaxed state to filepath: ", result_path/"m_relaxed")
