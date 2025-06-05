dd5879cd38517f1f721c2eb6e18fc3be58aede3a
wagerc97 <wager.clemens@gmail.com>
2025-06-05 12:32:39 +0200

    solved

--- micromag/exercises/fd/exchange.py
+++ micromag/exercises/fd/exchange.py
@@ -9,19 +9,20 @@ class ExchangeField(object):
 
         # initialize laplace kernel
         self._kernel = np.zeros((3,3,3))
-        # TODO setup 3D Laplace kernel
+        self._kernel[:,1,1] += np.array((1,-2,1)) / mesh.dx[0]**2
+        self._kernel[1,:,1] += np.array((1,-2,1)) / mesh.dx[1]**2
+        self._kernel[1,1,:] += np.array((1,-2,1)) / mesh.dx[2]**2
 
         # initialize scratch space
         self._h = np.zeros(mesh.n + (3,))
 
     def h(self, t, m):
-        # TODO
-        # Implement exchange field:
-        # 2 * A / (mu_0 * Ms) * Laplace(m)
-        # TIP: use ndimage.convolve with self._kernel
-        raise NotImplementedError
+        f = 2. * self._A / (constants.mu_0 * self._Ms)
+        for i in range(3):
+            self._h[:,:,:,i] = f * ndimage.convolve(m[:,:,:,i], self._kernel)
+
+        return self._h
 
     def E(self, t, m):
-        # TODO
-        # Implement exchange energy:
-        raise NotImplementedError
+        return - 0.5 * constants.mu_0 * self._mesh.cell_volume \
+               * np.sum(self._Ms * m * self.h(t, m))
